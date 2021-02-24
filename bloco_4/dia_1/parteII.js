7cf35824ae64c510c20877e8a1c1d2c6bcb5f251
Jander Liborio <jandernunes14@gmail.com>
2021-02-24 15:55:25 -0400

    teste no javascript

---
+++ bloco_4/dia_1/parteII.js
@@ -0,0 +1,15 @@
+let name = "jander liborio";
+const birthCity = "Manaus";
+let birthYear = 2000;
+
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2020;
+
+console.log(birthYear);
+
+birthCity = "sp";
+
+console.log(birthCity);

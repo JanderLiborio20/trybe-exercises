67e67edef254878af7c67338a68b6983295d0665
Jander Liborio <jandernunes14@gmail.com>
2021-02-26 15:13:03 -0400

    ex5 feito

---
+++ exercises/exercises/ex4.js
@@ -0,0 +1,9 @@
+let positivo = 20;
+let negativo = -20;
+let neutro = 0;
+
+if(negativo < 0){
+    console.log("negativo");
+}else{
+    console.log("positivo");
+}
---
+++ exercises/exercises/ex5.js
@@ -0,0 +1,13 @@
+let a = -60;
+let b = 60;
+let c = 60;
+
+let somatriangulo = a + b + c;
+
+if(somatriangulo == 180){
+    console.log("true");
+}else if(somatriangulo < 0){
+    console.log("erro");
+}else{
+    console.log("false");
+}

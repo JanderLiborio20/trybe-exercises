d33aa3abb498528fca558820ad276317f89f5fc3
Jander Liborio <jandernunes14@gmail.com>
2021-02-26 14:29:17 -0400

    ex3 feito

---
+++ exercises/exercises/ex3.js
@@ -0,0 +1,11 @@
+let comparadorA = 48;
+let comparadorB = 56;
+let comparadorC = 89;
+
+if(comparadorA > comparadorB && comparadorA > comparadorC){
+    console.log(comparadorA);
+}else if(comparadorB > comparadorA && comparadorB > comparadorC){
+    console.log(comparadorB);
+}else{
+    console.log(comparadorC); 
+}

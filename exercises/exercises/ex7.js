cc2d81e38790a60a52741a4644c2bcc13ee925d6
Jander Liborio <jandernunes14@gmail.com>
2021-03-01 15:01:01 -0400

    ex7 feito

---
+++ exercises/exercises/ex7.js
@@ -0,0 +1,16 @@
+let nota = 40;
+if(nota >= 90){
+  console.log("A");
+}else if(nota >= 80){
+  console.log("B");
+}else if(nota >=70){
+  console.log("C");
+}else if(nota >= 60){
+  console.log("D");
+}else if(nota >= 50){
+  console.log("E");
+}else if(nota < 50){
+  console.log("F");
+}else{
+  console.log("erro nota não encontrada");
+}
